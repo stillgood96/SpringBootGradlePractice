4f341875b7f6d90fba7a67ecf8efb36b25240321
a7875 <a787574@naver.com>
2021-12-12 21:11:34 +0900

    회원 웹 기능 - 등록

--- src/main/java/com/springPractice/practiceAlone/controller/MemberController.java
+++ src/main/java/com/springPractice/practiceAlone/controller/MemberController.java
@@ -1,26 +1,44 @@
 package com.springPractice.practiceAlone.controller;
 
+import com.springPractice.practiceAlone.domain.Member;
 import com.springPractice.practiceAlone.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
     // 빈의 DI 방법 1. 필드주입
-    @Autowired
+//    @Autowired
     private  MemberService memberService;
 
 //    // 빈의 DI 방법 2. 생성자주입 (가장 권장되는 방법)
-//    @Autowired
-//    public MemberController(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     // 빈의 DI 방법 3. Setter 주입
 //    @Autowired
 //    public void setMemberService(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
