8b7f6f2b31713b0a688dee91560dc654a0b5fb31
a7875 <a787574@naver.com>
2021-12-12 18:25:34 +0900

    회원서비스 테스트 MemberRepository Dependency Injection -> MemberRepository 객체를 두번생성하게 되는데 이는 하나의 객체에서 이뤄져야할 부분이 -> 또다른 객체가 생성되면서 불필요한 객체가 하나 더 생기는 문제이다. -> Service 클래스에서 생성자를 통해 Repository 객체를 new를 통해 생성하지 않고 -> Service 클래스가 생성자를 통해 인스턴스 될때 Repository 객체를 밀어 넣어주는 것 .

--- src/main/java/com/springPractice/practiceAlone/service/MemberService.java
+++ src/main/java/com/springPractice/practiceAlone/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
@@ -53,7 +57,7 @@ public class MemberService {
             return memberRepository.findAll();
     }
 
-    public Optional<Member> findONe (Long memberId) {
+    public Optional<Member> findOne (Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
---
+++ src/test/java/com/springPractice/practiceAlone/service/MemberServiceTest.java
@@ -0,0 +1,91 @@
+package com.springPractice.practiceAlone.service;
+
+import com.springPractice.practiceAlone.domain.Member;
+import com.springPractice.practiceAlone.repositroy.MemoryMemberRepository;
+import com.springPractice.practiceAlone.repostiroy.MemoryMemberRepositoryTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+
+    // 테스트 클래스 메서드명을 작성할때는 과감하게 한글로 작성해도 된다.
+    // 빌드 될때 테스트코드는 포함되지 않는다.
+    @Test
+    void 회원가입() {
+            // given
+            Member member = new Member();
+            member.setName("hello");
+
+            // when
+            Long saveId = memberService.join(member);
+
+            // then
+            Member findMember = memberService.findOne(saveId).get();
+            assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // 위에서 작성된 회원가입 테스트는 반쪽짜리 테스트이다.
+        // 회원가입에서 발생할 수 있는 이름중복에 대한 예외상황에 대해서도 테스트 코드가 작성되어야 한다.
+
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+            // 예외상황을 잡는 방법1. try / catch
+            try {
+                memberService.join(member2);
+                fail();
+            }catch(IllegalStateException e) {
+                // 예외상황 발생시 생기는 메세지가 동일한지 체크를 하는 것.
+                assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+            }
+
+            // 예외상황을 잡는 방법2.
+            // memberService.join(member2) 메서드를 실행할 건데 IllegalStateException 예외가 터져야 한다!
+            assertThrows(IllegalStateException.class, () -> {memberService.join(member2);});
+
+            // IllegalStateException오류 발생시 출력되는 메세지 검증방법
+            // assertThrows(IllegalStateException.class, () -> {memberService.join(member2);}); 작성 후  Ctrl + Alt + V 를 하면 객체에다 할당해줌
+            // 할당된 객체에 메세지를 assertThat을 통해 검증하면 된다.
+            IllegalStateException e = assertThrows(IllegalStateException.class, () -> { memberService.join(member2); });
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        // then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findONe() {
+    }
+}
