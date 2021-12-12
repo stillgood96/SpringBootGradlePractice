aac912b4a19370c75641e89e7434ba4909350d59
a7875 <a787574@naver.com>
2021-12-12 21:21:30 +0900

    회원 웹 기능 - 조회

--- src/main/java/com/springPractice/practiceAlone/controller/MemberController.java
+++ src/main/java/com/springPractice/practiceAlone/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.springPractice.practiceAlone.domain.Member;
 import com.springPractice.practiceAlone.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -41,4 +44,11 @@ public class MemberController {
         return "redirect:/";
     }
 
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
